697ec9e52c4509b1ba8552e82b1c779fb3cc14d3
Vitalii Karniushin <vkarnyushin@xebialabs.com>
2018-08-27 14:07:59 +0200

    123

--- XLRelease/T1.groovy
+++ XLRelease/T1.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://piter.local:5516/#/templates/Release6b86d7241149419e852cd814154b91e6/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Mon Aug 27 14:03:45 CEST 2018
+// Date created:         Mon Aug 27 14:07:58 CEST 2018
 
 xlr {
   template('T1') {
